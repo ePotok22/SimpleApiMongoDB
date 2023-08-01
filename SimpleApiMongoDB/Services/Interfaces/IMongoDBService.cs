fbf95281275b2c03638a5bf3346c195f83b7da87
KHAWFANG-SM\Administrator <kf.potok@gmail.com>
2023-08-01 10:46:14 +0700

    Update

--- SimpleApiMongoDB/Services/Interfaces/IMongoDBService.cs
+++ SimpleApiMongoDB/Services/Interfaces/IMongoDBService.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.Extensions.Options;
-using MongoDB.Bson;
-using MongoDB.Driver;
-using SimpleApiMongoDB.Models;
-using SimpleApiMongoDB.Entities;
+﻿using SimpleApiMongoDB.Entities;
 
 namespace SimpleApiMongoDB.Services.Interfaces
 {
